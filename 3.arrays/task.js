d80204dbf7e2a05fb926007e788a8a1da5d27e8b
Valeria Mishurova <valserdenisova@gmail.com>
2025-09-03 23:37:47 +0300

    Задание Массивы сделано

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
-  
+	return (
+		arr1.length === arr2.length &&
+		arr1.every((item, index) => item === arr2[index])
+	);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	return users.filter(user => user.gender === gender)
+        .map(user => user.age)
+        .reduce((acc, age, index, arr) => acc + age / arr.length, 0);
 }
