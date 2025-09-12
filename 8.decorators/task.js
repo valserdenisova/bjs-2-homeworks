2fa647c60ea4f6f0f7eac6654a09e715c7a28f38
Valeria Mishurova <valserdenisova@gmail.com>
2025-09-12 21:03:35 +0300

    Задание выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,42 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
-}
+    let cache = [];
+    function wrapper(...args) {
+      const hash = md5(args);
+      let objectInCache = cache.find( item => item.hash === hash);
+      if (objectInCache) {
+        console.log("Из кеша: " + objectInCache.value);
+        return "Из кеша: " + objectInCache.value;
+      }
+      let result = func(...args);
+      cache.push({"hash": hash, "value": result});
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+    return wrapper;
+  }
 
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
-}
+  //Задача № 2
+  function debounceDecoratorNew(func, delay) {
+    let timeoutId;
+    let isTrottled = false;
+    function wrapper(...args) {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout( () => {
+        func(args);
+        wrapper.count++;
+      }, delay);
+      if (!isTrottled) {
+        func(...args);
+        wrapper.count++;
+        isTrottled = true;
+      }
+      wrapper.allCount++;
+    }
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
+  }
